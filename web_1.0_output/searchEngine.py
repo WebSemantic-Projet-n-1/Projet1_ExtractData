294d1bce5d6de1d3f88a9c7399e633cba308cc23
AlexBOYERE <Proalexandreboyere@gmail.com>
2026-02-01 17:36:27 -0500

    Requête r1 sur le script

--- web_1.0_output/searchEngine.py
+++ web_1.0_output/searchEngine.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 # Utilise beautifulsoup permettant de filtrer avec les balises
 # https://beautiful-soup-4.readthedocs.io/en/latest/#output
 def getContentByUrl(url):
+
+    print(url)
+
     with open(url, 'r', encoding='utf-8') as f:
         content = f.read()
 
@@ -14,6 +17,7 @@ def getContentByUrl(url):
         print('content: ' + soup.prettify())
         return soup
     else:
+        print('content echec')
         return None
 
 # GET = Title, WITH = soup
@@ -26,6 +30,43 @@ def getTitleFromSoup(soup):
         print('title: ' + title)
         return title
     else:
+        print('title echec')
         return None
 
-getTitleFromSoup(getContentByUrl('classement.html'))
+
+# GET = firstRow usually, WITH soup
+# Récupère la première ligne utile (hors en tête) dans un table
+# https://stackoverflow.com/questions/39440830/attributeerror-nonetype-object-has-no-attribute-find-all-many-of-the-other
+def getFirstRowInArray(soup):
+    rows = soup.find('table').find_all('tr')
+
+    # On vérifie qu'il a bien récupéré au moins deux lignes
+    if len(rows) >= 2:
+        # on récupère la seconde ligne qu'on appelle firstRow car c'est la première de valeur
+        firstRow = rows[1]
+
+        return firstRow
+
+    return None
+
+
+# Réponse R1
+def getFirstTeamInClassment():
+    url = 'classement.html'
+
+    soup = getContentByUrl(url)
+
+    firstRow = getFirstRowInArray(soup)
+
+    nameOfTheTeam = firstRow.find_all('td')[1].string
+
+    if nameOfTheTeam is not None:
+        print('nameOfTheTeam: ' + nameOfTheTeam)
+        return nameOfTheTeam
+
+    return None
+
+
+
+# R1
+# getFirstTeamInClassment()
