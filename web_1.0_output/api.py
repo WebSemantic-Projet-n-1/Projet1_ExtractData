7728cf308230f18f3bd5ce79b62704ad793535d1
AlexBOYERE <Proalexandreboyere@gmail.com>
2026-02-04 10:45:33 -0500

    api first request

--- web_1.0_output/api.py
+++ web_1.0_output/api.py
@@ -1,4 +1,4 @@
-from typing import Union
+import searchEngine
 
 from fastapi import FastAPI
 
@@ -9,11 +9,33 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/requests/{request_question}")
+def read_request(request_question: str):
 
-@app.get("/requests/{request_id}")
-def read_request(request_id: int, title: str, answer: str):
+    datas = []
+
+    rules = [
+        (['première', 'classement'], "Question 1", searchEngine.getFirstTeamInClassment()),
+        (['matchs', 'joués', 'saison'], "Question 2", "Réponse"),
+        (['nombre', 'total', 'buts', 'saison'], "Question 3", "Réponse"),
+        (['équipe', 'marqué', 'le plus de buts'], "Question 4", "Réponse"),
+        (['équipes', 'marqué', 'plus de 70 buts', 'saison'], "Question 5", "Réponse"),
+        (['matchs', 'novembre 2008'], "Question 6", "Réponse"),
+        (['victoires', 'domicile', 'Manchester', 'United'], "Question 7", "Réponse"),
+        (['classement', 'équipes', 'nombre', 'victoires', 'extérieur'], "Question 8", "Réponse"),
+        (['moyenne', 'buts marqués', 'extérieur', 'équipes', 'top 6'], "Question 9", "Réponse"),
+        (['confrontations', 'historiques', 'championnat'], "Question 10", "Réponse"),
+    ]
+
+    # Filtrage générique
+    for words, title, answer in rules:
+        if all(word in request_question for word in words):
+            datas.append({
+                "title": title,
+                "answer": answer
+            })
+
     return {
-        "request_id": request_id,
-        "title": title,
-        "answer": answer
+        "request_question": request_question,
+        "datas": datas,
     }
