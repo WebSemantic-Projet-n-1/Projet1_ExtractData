d4475c85cc98c01ee37ecc65ecafc906fab1b8ec
Victor <34606799+T4rg3n@users.noreply.github.com>
2026-02-12 16:56:04 -0500

    Add R1 from KG (#74)

---
+++ engine/knowledge_graph.py
@@ -0,0 +1,35 @@
+"""
+RDF database operations
+"""
+
+import rdflib
+
+g = rdflib.Graph()
+g.parse("knowledge_graph.ttl", format="turtle")
+
+def getFirstTeamInClassment():
+    """R1 - First team in ranking using Knowledge Graph SPARQL query.
+
+    Executes a SPARQL query over the global RDF graph to retrieve the name
+    of the sports team whose position is "1" in the ranking.
+
+    Returns:
+        str | None: The name of the first-ranked team if found, otherwise None.
+    """
+    query = """
+    PREFIX schema1: <http://schema.org/>
+    SELECT ?teamName ?position
+    WHERE {
+        ?sportsTeam a schema1:SportsTeam .
+        ?sportsTeam schema1:position ?position .
+        ?sportsTeam schema1:name ?teamName .
+        FILTER(?position = "1").
+    }
+    LIMIT 1
+    """
+    results = g.query(query)
+    
+    for row in results:
+        return row.teamName
+    
+    return None
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, RedirectResponse
 from api.api_web_1 import router as api_web_1_0_router
 from api.api_rdfa import router as api_rdfa_router
 from api.api_knowledge_graph import router as api_knowledge_graph_router
@@ -9,12 +9,12 @@ app = FastAPI()
 # Root endpoint
 @app.get("/")
 async def read_root():
-    return FileResponse("pages/index.html")
+    return RedirectResponse(url="/web-1.0")
 
 # Search Engine Web 1.0
 @app.get("/web-1.0/")
 async def v1():
-    return FileResponse("pages/searchEngine_web-1.0.html")
+    return FileResponse("pages/search_web1.html")
 
 # API Web 1.0
 app.include_router(api_web_1_0_router)
@@ -22,7 +22,7 @@ app.include_router(api_web_1_0_router)
 # Search Engine RDFa
 @app.get("/rdfa/")
 async def v2():
-    return FileResponse("pages/searchEngine_rdfa.html")
+    return FileResponse("pages/search_rdfa.html")
 
 # API RDFa
 app.include_router(api_rdfa_router)
@@ -30,7 +30,7 @@ app.include_router(api_rdfa_router)
 # Search Engine Knowledge Graph
 @app.get("/knowledge-graph/")
 async def v3():
-    return FileResponse("pages/searchEngine_knowledge-graph.html")
+    return FileResponse("pages/search_knowledge-graph.html")
 
 # API Knowledge Graph
 app.include_router(api_knowledge_graph_router)
